2f5cc3544eb026d52bdd973b483541f377ca6267
sophie <sophie.human.k@gmail.com>
2021-11-30 18:59:48 +0900

    store 모듈 변경

--- src/store/index.js
+++ src/store/index.js
@@ -1,39 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import main from './main'
-import keywordSearch from './keywordSearch'
-import actorDetail from './actorDetail'
-import round from './round'
-import detail from './detail'
-import keyword from './keyword'
-import actor from './actor'
-import tvToday from './tvToday'
-import tvNow from './tvNow'
-import movieNow from './movieNow'
-import topRated from './topRated'
-import trending from './trending'
-import popular from './popular'
-import movieUpcoming from './movieUpcoming'
-import tvmoviesearch from './tvmoviesearch'
+import modules from './modules'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules: {
-    main,
-    keywordSearch,
-    actorDetail,
-    round,
-    detail,
-    keyword,
-    actor,
-    tvToday,
-    tvNow,
-    movieNow,
-    topRated,
-    trending,
-    popular,
-    movieUpcoming,
-    tvmoviesearch
-  }
+  modules
 })
--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,18 +1,18 @@
-import main from '../main'
-import keywordSearch from '../keywordSearch'
-import actorDetail from '../actorDetail'
-import round from '../round'
-import detail from '../detail'
-import keyword from '../keyword'
-import actor from '../actor'
-import tvToday from '../tvToday'
-import tvNow from '../tvNow'
-import movieNow from '../movieNow'
-import topRated from '../topRated'
-import trending from '../trending'
-import popular from '../popular'
-import movieUpcoming from '../movieUpcoming'
-import tvmoviesearch from '../tvmoviesearch'
+import main from './main'
+import keywordSearch from './keywordSearch'
+import actorDetail from './actorDetail'
+import round from './round'
+import detail from './detail'
+import keyword from './keyword'
+import actor from './actor'
+import tvToday from './tvToday'
+import tvNow from './tvNow'
+import movieNow from './movieNow'
+import topRated from './topRated'
+import trending from './trending'
+import popular from './popular'
+import movieUpcoming from './movieUpcoming'
+import tvmoviesearch from './tvmoviesearch'
 
 export default {
   main,
@@ -25,7 +25,7 @@ export default {
   tvToday,
   tvNow,
   movieNow,
-  topRated,
+  topRated,경
   trending,
   popular,
   movieUpcoming,
